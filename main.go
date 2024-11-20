f86d1a0ebafdecd91ba697d56ca8365c79bfefd8
uzengo <buct.luhao@gmail.com>
2024-11-20 12:07:38 +0800

    支持svc

--- main.go
+++ main.go
@@ -22,7 +22,10 @@ import (
 )
 
 var (
-	line = liner.NewLiner()
+	line       = liner.NewLiner()
+	kubeConfig *string
+	namespace  *string = new(string)
+	k8sClient  *kubernetes.Clientset
 )
 
 func main() {
@@ -31,23 +34,23 @@ func main() {
 	line.SetCtrlCAborts(true)
 
 	// 获取配置文件路径
-	kubeconfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	namespace := flag.String("namespace", "", "k8s namespace to use")
+	kubeConfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	namespace = flag.String("namespace", "", "k8s namespace to use")
 	flag.Parse()
 	// 配置文件不能为空
-	if *kubeconfig == "" {
+	if *kubeConfig == "" {
 		fmt.Println("Kubeconfig file is required")
 		return
 	}
 
 	// 使用配置文件创建k8s客户端
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
 	if err != nil {
 		fmt.Printf("Error building kubeconfig: %v\n", err)
 		return
 	}
 
-	clientset, err := kubernetes.NewForConfig(config)
+	k8sClient, err = kubernetes.NewForConfig(config)
 	if err != nil {
 		fmt.Printf("Error creating Kubernetes client: %v\n", err)
 		return
@@ -56,7 +59,7 @@ func main() {
 	for {
 		if *namespace == "" {
 			//获取k8s命名空间
-			namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+			namespaces, err := k8sClient.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 			if err != nil {
 				fmt.Printf("Error listing namespaces: %v\n", err)
 				fmt.Printf("获取命名空间列表失败，请检查是否有权限，可手动指定命名空间")
@@ -77,72 +80,76 @@ func main() {
 				return
 			}
 		}
-		handleNamespaceAction(clientset, kubeconfig, namespace)
+		var action = new(string)
+		prompt := &survey.Select{
+			Message: fmt.Sprintf("choose action in namespace %s:", *namespace),
+			Options: []string{"pods", "svc", "configmap", "exit"},
+		}
+		err = survey.AskOne(prompt, action)
+		if err != nil {
+			fmt.Printf("Error selecting action: %v\n", err)
+			os.Exit(1)
+			return
+		}
+		switch *action {
+		case "pods":
+			handleNamespacePodAction()
+		case "svc":
+			handleNamespaceSvcAction()
+		case "configmap":
+			handleNamespaceConfigMapAction()
+		case "exit":
+			fmt.Println("bye!!!")
+			os.Exit(0)
+		}
 
 	}
 
-	// // 获取Pod列表
-	// pods, err := clientset.CoreV1().Pods(*namespace).List(context.TODO(), metav1.ListOptions{})
-	// if err != nil {
-	// 	fmt.Printf("Error listing pods: %v\n", err)
-	// 	return
-	// }
-
-	// // 显示Pod列表并标号
-	// fmt.Println("Pods in namespace", *namespace)
-	// for i, pod := range pods.Items {
-	// 	fmt.Printf("[\u001B[1;31m %d \u001B[0m] %s \u001B[0;32m%s\u001B[0m \n", i, pod.Name, pod.Status.Phase)
-	// }
-
-	// for {
-
-	// 	fmt.Println("Enter exit to quit")
-	// 	// 用户输入编号 或者 继续搜索
-	// 	//reader := bufio.NewReader(os.Stdin)
-	// 	//fmt.Print("Enter pod number or search: ")
-
-	// 	input, err := line.Prompt("Enter pod number or search: ")
-	// 	if err != nil {
-	// 		fmt.Printf("Error reading input: %v\n", err)
-	// 		return
-	// 	}
-	// 	//input, _ = reader.ReadString('\n')
-	// 	input = strings.TrimSpace(input)
-	// 	if input != "" {
-	// 		line.AppendHistory(input)
-	// 	}
-
-	// 	// 检查输入是否为数字
-	// 	podNumber, err := strconv.Atoi(input)
-	// 	if err == nil && podNumber >= 0 && podNumber < len(pods.Items) {
-	// 		selectedPod := pods.Items[podNumber]
-	// 		handlePodAction(line, selectedPod, kubeconfig, namespace)
-	// 	} else {
-	// 		//如果== exit 退出
-	// 		if input == "exit" {
-	// 			fmt.Println("bye bye")
-	// 			os.Exit(0)
-	// 		}
-	// 		// 搜索Pod名称
-	// 		fmt.Println("Searching for pods containing:", input)
-	// 		for i, pod := range pods.Items {
-	// 			if strings.Contains(pod.Name, input) {
-	// 				fmt.Printf("[\u001B[1;31m %d \u001B[0m] %s \u001B[0;32m%s\u001B[0m \n", i, pod.Name, pod.Status.Phase)
-	// 			}
-	// 		}
-	// 	}
-
-	// 	pods, err = clientset.CoreV1().Pods(*namespace).List(context.TODO(), metav1.ListOptions{})
-	// 	if err != nil {
-	// 		fmt.Printf("Error listing pods: %v\n", err)
-	// 		return
-	// 	}
-	//}
 }
 
-func handleNamespaceAction(clientset *kubernetes.Clientset, kubeconfig *string, namespace *string) {
+func handleNamespaceConfigMapAction() {
+	panic("unimplemented")
+}
+
+func handleNamespaceSvcAction() {
+	// 获取Service列表
+	svcs, err := k8sClient.CoreV1().Services(*namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error listing services: %v\n", err)
+		fmt.Printf("获取命名空间%s下的Service列表失败", *namespace)
+		return
+	}
+	// 打印Service列表
+	fmt.Println("Services in namespace", *namespace)
+	printSvcTable(svcs, "", nil)
+	for {
+		input := ""
+		prompt := &survey.Input{
+			Message: "Enter pod number or search, exit to quit: ",
+		}
+		survey.AskOne(prompt, &input)
+
+		// 	// 检查输入是否为数字
+		podNumber, err := strconv.Atoi(input)
+		if err == nil && podNumber >= 0 && podNumber < len(svcs.Items) {
+			selectedSvc := svcs.Items[podNumber]
+			handleSvcAction(line, selectedSvc)
+		} else {
+			//如果== exit 退出
+			if input == "exit" {
+				return
+			}
+			printSvcTable(svcs, input, func(pod v1.Service, input string) bool {
+				return strings.Contains(pod.Name, input)
+			})
+		}
+	}
+
+}
+
+func handleNamespacePodAction() {
 	// 获取Pod列表
-	pods, err := clientset.CoreV1().Pods(*namespace).List(context.TODO(), metav1.ListOptions{})
+	pods, err := k8sClient.CoreV1().Pods(*namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		fmt.Printf("Error listing pods: %v\n", err)
 		fmt.Printf("获取命名空间%s下的Pod列表失败", *namespace)
@@ -167,12 +174,11 @@ func handleNamespaceAction(clientset *kubernetes.Clientset, kubeconfig *string, 
 		podNumber, err := strconv.Atoi(input)
 		if err == nil && podNumber >= 0 && podNumber < len(pods.Items) {
 			selectedPod := pods.Items[podNumber]
-			handlePodAction(line, selectedPod, kubeconfig, namespace)
+			handlePodAction(line, selectedPod)
 		} else {
 			//如果== exit 退出
 			if input == "exit" {
-				fmt.Println("bye!!!")
-				os.Exit(0)
+				return
 			}
 			// 搜索Pod名称
 			//fmt.Println("Searching for pods containing:", input)
@@ -201,7 +207,65 @@ func printPodTable(pods *v1.PodList, input string, f func(pod v1.Pod, input stri
 	table.Render()
 }
 
-func handlePodAction(line *liner.State, pod v1.Pod, kubeconfig, namespace *string) {
+func printSvcTable(pods *v1.ServiceList, input string, f func(pod v1.Service, input string) bool) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Number", "Name", "TYPE", "CLUSTER-IP", "EXTERNAL-IP", "PORT(S)"})
+	for i, pod := range pods.Items {
+		if f != nil && !f(pod, input) {
+			continue
+		}
+		var ports []string = make([]string, 0)
+		for _, port := range pod.Spec.Ports {
+			ports = append(ports, fmt.Sprintf("%d/%s", port.Port, port.Protocol))
+		}
+		table.Append([]string{
+			fmt.Sprintf("%d", i),
+			pod.Name,
+			string(pod.Spec.Type),
+			pod.Spec.ClusterIP,
+			strings.Join(pod.Spec.ExternalIPs, ","),
+			strings.Join(ports, ","),
+		})
+	}
+	table.Render()
+}
+
+func handleSvcAction(line *liner.State, svc v1.Service) {
+	for {
+		fmt.Println("====================================")
+		// 高亮显示选中的svc名称
+		fmt.Printf("Selected svc: \033[1;33m %s \033[0m \n", svc.Name)
+		fmt.Println("====================================")
+		fmt.Println("command action [p fw q]: ")
+		fmt.Println("\u001B[0;31m p \u001B[0m: print svc info")
+		fmt.Println("\u001B[0;31m fw \u001B[0m: forward svc port")
+		fmt.Println("\u001B[0;31m q \u001B[0m: quit")
+
+		action, _ := line.Prompt("Enter action: ")
+		action = strings.TrimSpace(action)
+
+		switch action {
+		case "p":
+			cmd := exec.Command("kubectl", "--kubeconfig", *kubeConfig, "-n", *namespace, "get", "svc", svc.Name, "-o", "yaml")
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
+		case "fw":
+			ports, _ := line.Prompt("please enter forward ports, example: local port:svc port, you can input 8080:80 : ")
+			ports = strings.TrimSpace(ports)
+			cmd := exec.Command("kubectl", "--kubeconfig", *kubeConfig, "-n", *namespace, "port-forward", "svc/"+svc.Name, ports)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
+		case "q":
+			return
+		default:
+			fmt.Println("Invalid action")
+		}
+	}
+}
+
+func handlePodAction(line *liner.State, pod v1.Pod) {
 	fmt.Println("====================================")
 	// 高亮显示选中的Pod名称
 	fmt.Printf("Selected pod: \033[1;33m %s \033[0m \n", pod.Name)
@@ -218,19 +282,19 @@ func handlePodAction(line *liner.State, pod v1.Pod, kubeconfig, namespace *strin
 	switch action {
 	case "l":
 		// 查看日志
-		cmd := exec.Command("kubectl", "--kubeconfig", *kubeconfig, "-n", *namespace, "logs", pod.Name)
+		cmd := exec.Command("kubectl", "--kubeconfig", *kubeConfig, "-n", *namespace, "logs", pod.Name)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
 	case "lf":
 		// 查看滚动日志
-		cmd := exec.Command("kubectl", "--kubeconfig", *kubeconfig, "-n", *namespace, "logs", "-f", "--tail=1000", pod.Name)
+		cmd := exec.Command("kubectl", "--kubeconfig", *kubeConfig, "-n", *namespace, "logs", "-f", "--tail=1000", pod.Name)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		cmd.Run()
 	case "s":
 		// 进入shell
-		cmd := exec.Command("kubectl", "--kubeconfig", *kubeconfig, "-n", *namespace, "exec", "-it", pod.Name, "--", "/bin/bash")
+		cmd := exec.Command("kubectl", "--kubeconfig", *kubeConfig, "-n", *namespace, "exec", "-it", pod.Name, "--", "/bin/bash")
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
